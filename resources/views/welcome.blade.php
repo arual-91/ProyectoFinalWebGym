625929cadd5dc0df544cd38cc3f8b7af5f67f284
Arual <andrades.robles@gmail.com>
2021-05-03 17:03:35 +0200

    Inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Gym</title>
+        <style>
+          
+        </style>
+    </head>
+    <body >
+    </body>
+</html>
